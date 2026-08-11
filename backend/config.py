d024f9ef3c61bd2d22f001c93b9901dcdae5f414
Rintarou Okabe <utkarshpinjare40@gmail.com>
2026-08-12 04:11:44 +0530

    :dynamic api values and observer value read from environment

--- backend/config.py
+++ backend/config.py
@@ -2,12 +2,15 @@
 Central configuration for the satellite tracker backend.
 Edit OBSERVER_* to your actual ground station location before running anything.
 """
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Observer location (used for Az/El calculations)
 # one defauld location is already set init.
-OBSERVER_LAT_DEG = 19.1335 
-OBSERVER_LON_DEG = 72.9092
-OBSERVER_ELEVATION_M = 51  # this is in meters; above sea level (approx)
+# after upadated cod eit eill reaf from .env file environment.
+OBSERVER_LAT_DEG = float(os.environ.get("OBSERVER_LAT_DEG"))
+OBSERVER_LON_DEG = float(os.environ.get("OBSERVER_LON_DEG"))
+OBSERVER_ELEVATION_M = float(os.environ.get("OBSERVER_ELEVATION_M"))  # this is in meters; above sea level (approx)
 
 # Celestrak site TLE fetch settings 
 CELESTRAK_BASE_URL = "https://celestrak.org/NORAD/elements/gp.php"
@@ -26,4 +29,4 @@ TLE_REFRESH_INTERVAL_HOURS = 6
 TLE_CACHE_DIR = "tle/cache"
 # API server settings
 API_HOST = "0.0.0.0"
-API_PORT = 7789
+API_PORT = int(os.environ.get("PORT", 7789))
